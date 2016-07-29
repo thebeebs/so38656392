cd3090f9e93c439154998834092634791029b524
thebeebs <martinbeeby@hotmail.com>
2016-07-29 14:54:49 +0100

    Add project files.

---
+++ src/so38656392/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using so38656392.Models;
+
+namespace so38656392.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // I'm using ASP.net Core MVC with Razor. 
+            // My ViewModel contains string OriginalText and 
+            // List<string> KeyPhrases. I already have a CSS class 
+            // called highlight which will apply the yellow 
+            // colour as a background-color.
+
+            var vm = new ResultViewModel();
+            vm.OriginalText = "I wonder is this Cognitive Services API stuff actually works? Lets hope so";
+            vm.KeyPhrases = new List<String>();
+            vm.KeyPhrases.Add("API stuff");
+            vm.KeyPhrases.Add("works");
+            vm.KeyPhrases.Add("Lets");
+
+            return View(vm);
+        }
+
+        public IActionResult About()
+        {
+            
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/so38656392/Models/ResultViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace so38656392.Models
+{
+   public class ResultViewModel
+    {
+        public ResultViewModel()
+        {
+        }
+
+        public List<string> KeyPhrases { get; internal set; }
+        public string OriginalText { get; internal set; }
+
+        private string myVar;
+
+        public string MarkText()
+        {
+            string text = this.OriginalText;
+            foreach (string word in KeyPhrases)
+            {
+                text = text.Replace(word, string.Format("<mark>{0}</mark>",word));
+            }
+            return text;
+
+        }        
+
+    }
+}
